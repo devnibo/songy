c11912befb4d692becc8801bdf732c0ce3199b65
Robin <kroekerrobin@gmail.com>
2023-02-19 18:44:58 +0100

    Config stuff

--- build_for_release.sh
+++ build_for_release.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # Using musl means everything is compiled statically
 # which results in an independant binary be able to
 # run in on any x86_64 based system. (That's how I understand it)
 read -p "Version Number (e.g. v1.2.3): " versionNumber
-cmds[0]="git tag -a $versionNumber -m $versionNumber"
-cmds[1]="cargo build -r --target x86_64-unknown-linux-musl"
-cmds[2]="cp target/x86_64-unknown-linux-musl/release/songs ."
-cmds[3]="mv songs songs_$versionNumber_x86_64-unknown-linux-musl"
+declare -a cmds=(
+	"git tag -a $versionNumber -m '$versionNumber'"
+	"cargo build -r --target x86_64-unknown-linux-musl"
+	"cp target/x86_64-unknown-linux-musl/release/songs ."
+	"mv songs songs_"$versionNumber"_x86_64-unknown-linux-musl"
+)
+for (( i=0; i<${#cmds[@]}; i++ ));
+do
+	echo "${cmds[$i]}"
+	bash -c "${cmds[$i]}"
+done
