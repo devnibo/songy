64a9e7687d511110f7701b3943ae6d41dfcb27b2
devrobinkroeker <kroekerrobin@gmail.com>
2023-04-22 23:03:20 +0200

    Add a little helper in build_for_release.sh

--- build_for_release.sh
+++ build_for_release.sh
@@ -4,11 +4,12 @@
 # which results in an independant binary be able to
 # run in on any x86_64 based system. (That's how I understand it)
 read -p "Version Number (e.g. v1.2.3): " versionNumber
+test ${versionNumber:0:1} != "v" && versionNumber="v""$versionNumber"
 declare -a cmds=(
 	"git tag $versionNumber"
 	"cargo build -r --target x86_64-unknown-linux-musl"
 	"cp target/x86_64-unknown-linux-musl/release/songs ."
-	"mv songs songs_v"$versionNumber"_x86_64-unknown-linux-musl"
+	"mv songs songs_"$versionNumber"_x86_64-unknown-linux-musl"
 )
 for (( i=0; i<${#cmds[@]}; i++ ));
 do
